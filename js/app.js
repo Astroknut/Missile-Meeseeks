3c22efac32f04c4ca9c48da5ae0bb83896656ff8
Astroknut <charlesbarrett1521@gmail.com>
2017-07-12 10:57:23 -0600

    Adds social media logos/style update landing page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 window.onload = function() {
 	console.log("DOM is Ready!");
 };
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+function draw() {
+	ctx.fillStyle = 'rgb(200, 0 , 0)';
+	ctx.fillRect(10, 10, 50, 50);
+
+	ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+	ctx.fillRect(300, 300, 50, 50);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+draw();
