3ce38d9248abda4d79cb8aeba5cb8f880d4769cf
Astroknut <charlesbarrett1521@gmail.com>
2017-07-12 15:57:13 -0600

    adds canvas background/draws locations of player items

--- js/app.js
+++ js/app.js
@@ -6,11 +6,25 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
 function draw() {
-	ctx.fillStyle = 'rgb(200, 0 , 0)';
-	ctx.fillRect(10, 10, 50, 50);
 
-	ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-	ctx.fillRect(300, 300, 50, 50);
+		/*Build Land Area*/
+	ctx.fillStyle = '#668d3c';
+	ctx.fillRect(0, 450, 700, 50);
+
+		/*Base Locations*/
+	ctx.fillStyle = 'rgb(200, 0 , 0)';
+	ctx.fillRect(60, 430, 40, 30);
+	ctx.fillRect(160, 430, 40, 30);
+	ctx.fillRect(260, 430, 40, 30);
+	ctx.fillRect(380, 430, 40, 30);
+	ctx.fillRect(480, 430, 40, 30);
+	ctx.fillRect(580, 430, 40, 30);
+
+		/*Missile Silo Locations*/
+	ctx.fillStyle = 'rgb(0, 0, 200)';
+	ctx.fillRect(10, 430, 30, 30);
+	ctx.fillRect(325, 430, 30, 30);
+	ctx.fillRect(640, 430, 30, 30);	
 }
 
 
